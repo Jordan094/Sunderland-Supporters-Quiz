d3f7ce6124cddfcdba383183a0b651a0b02ac2c7
Jordan094 <>
2023-10-14 14:08:42 +0000

    UPDATED Change event listeners to if/else statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,54 @@
-// Get references to the buttons for the event listeners
+// Get references to the buttons and elements
 const instructionsButton = document.getElementById("show-instructions");
+const highScoresButton = document.getElementById("high-scores-button");
 const instructionsElement = document.querySelector(".quiz-instructions");
 const playGameButton = document.querySelector(".play-game");
-const highScoresButton = document.querySelector(".high-scores");
-
-// Add a click event listener to the "Instructions" button
-instructionsButton.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    // Hide other buttons
-    playGameButton.style.display = "none";
-    highScoresButton.style.display = "none";
-    instructionsButton.style.display = "none";
-
-    // Show the instructions element
-    instructionsElement.style.display = "block";
-});
-
-// Add a click event listener to the "Return" button within the instructions element
-const returnButton = document.getElementById("return-button");
-returnButton.addEventListener("click", function () {
-    // Show other buttons
-    playGameButton.style.display = "block";
-    highScoresButton.style.display = "block";
-    instructionsButton.style.display = "inline";
-
-    // Hide the instructions element when the "Return" button is clicked
-    instructionsElement.style.display = "none";
-});
-
-// Add a click event listener to the "High Scores" button
-highScoresButton.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    // Hide other buttons and elements
-    playGameButton.style.display = "none";
-    highScoresButton.style.display = "none";
-    instructionsButton.style.display = "none";
-    instructionsElement.style.display = "none";
-
-    // Show the high scores table
-    const highScoresTable = document.querySelector(".high-scores-table");
-    highScoresTable.style.display = "block";
-
-    // Call a function to populate the high scores table
-    populateHighScoresTable();
-});
-
-// Add a click event listener to the "Return" button within the high scores table
+const returnFromInstructionsButton = document.getElementById("return-from-instructions");
 const returnFromHighScoresButton = document.getElementById("return-from-high-scores");
-returnFromHighScoresButton.addEventListener("click", function () {
-    // Show other buttons
-    playGameButton.style.display = "block";
-    highScoresButton.style.display = "block";
-    instructionsButton.style.display = "inline";
+const highScoresTable = document.querySelector(".high-scores-table");
 
-    // Hide the high scores table when the "Return" button is clicked
-    const highScoresTable = document.querySelector(".high-scores-table");
-    highScoresTable.style.display = "none";
+// Add a click event listener to the buttons
+document.addEventListener("click", function (event) {
+    if (event.target === instructionsButton) {
+        event.preventDefault();
+
+        // Hide other buttons and elements
+        playGameButton.style.display = "none";
+        highScoresButton.style.display = "none";
+        instructionsButton.style.display = "none";
+        highScoresTable.style.display = "none";
+
+        // Show the instructions element
+        instructionsElement.style.display = "block";
+    } else if (event.target === highScoresButton) {
+        event.preventDefault();
+
+        // Hide other buttons and elements
+        playGameButton.style.display = "none";
+        highScoresButton.style.display = "none";
+        instructionsButton.style.display = "none";
+        instructionsElement.style.display = "none";
+
+        // Show the high scores table
+        highScoresTable.style.display = "block";
+
+        // Call a function to populate the high scores table
+        populateHighScoresTable();
+    } else if (event.target === returnFromInstructionsButton || event.target === returnFromHighScoresButton) {
+        event.preventDefault();
+
+        // Show other buttons
+        playGameButton.style.display = "inline";
+        highScoresButton.style.display = "inline";
+        instructionsButton.style.display = "inline";
+
+        // Hide the instructions element or high scores table when the "Return" button is clicked
+        instructionsElement.style.display = "none";
+        highScoresTable.style.display = "none";
+    }
 });
 
-// Function to populate the high scores table Placeholder data used at moment.
+// Function to populate the high scores table (replace with your actual data)
 function populateHighScoresTable() {
     const highScoresTable = document.getElementById("high-scores-table");
     const highScoresData = [
