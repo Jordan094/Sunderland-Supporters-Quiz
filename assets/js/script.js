61ea8728037b28fc38bc42582fa0c72cab40e018
Jordan094 <>
2023-10-14 13:16:26 +0000

    UPDATE Added function for highscores button

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,60 @@ returnButton.addEventListener("click", function () {
     // Hide the instructions element when the "Return" button is clicked
     instructionsElement.style.display = "none";
 });
+
+// Add a click event listener to the "High Scores" button
+highScoresButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Hide other buttons and elements
+    playGameButton.style.display = "none";
+    highScoresButton.style.display = "none";
+    instructionsButton.style.display = "none";
+    instructionsElement.style.display = "none";
+
+    // Show the high scores table
+    const highScoresTable = document.querySelector(".high-scores-table");
+    highScoresTable.style.display = "block";
+
+    // Call a function to populate the high scores table
+    populateHighScoresTable();
+});
+
+// Add a click event listener to the "Return" button within the high scores table
+const returnFromHighScoresButton = document.getElementById("return-from-high-scores");
+returnFromHighScoresButton.addEventListener("click", function () {
+    // Show other buttons
+    playGameButton.style.display = "block";
+    highScoresButton.style.display = "block";
+    instructionsButton.style.display = "inline";
+
+    // Hide the high scores table when the "Return" button is clicked
+    const highScoresTable = document.querySelector(".high-scores-table");
+    highScoresTable.style.display = "none";
+});
+
+// Function to populate the high scores table Placeholder data used at moment.
+function populateHighScoresTable() {
+    const highScoresTable = document.getElementById("high-scores-table");
+    const highScoresData = [
+        { username: "User1", score: 10 },
+        { username: "User2", score: 8 },
+        { username: "User3", score: 12 },
+    ];
+
+    // Clear existing table rows
+    highScoresTable.innerHTML = "<tr><th>Username</th><th>Score</th></tr>";
+
+    // Add rows for each high score entry
+    highScoresData.forEach((entry) => {
+        const row = document.createElement("tr");
+        const usernameCell = document.createElement("td");
+        usernameCell.textContent = entry.username;
+        const scoreCell = document.createElement("td");
+        scoreCell.textContent = entry.score;
+
+        row.appendChild(usernameCell);
+        row.appendChild(scoreCell);
+        highScoresTable.appendChild(row);
+    });
+}
